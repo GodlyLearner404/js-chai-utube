bd54f1ff8af18ce28e64308b1b365d562df2e51d
Gurnoor Singh Brar <157210409+GodlyLearner404@users.noreply.github.com>
2025-01-12 17:44:20 +0000

    const and var

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+// node 01_Basics/variables.js
+const accountId = 13189
+let accountEmail = "gurnoor@gmail.com"
+var accountPassword = "Master@13189"
+accountCity = "Chandigarh"
+
+// accountId = 24461
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity])

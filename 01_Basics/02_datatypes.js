a6a0e32bb6e009fc1c6343de63535c3532376664
Gurnoor Singh Brar <157210409+GodlyLearner404@users.noreply.github.com>
2025-01-12 17:44:46 +0000

    typeofs

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,7 @@
+// node 01_Basics/02_datatypes.js
+"use strict";//treat code as newer version
+
+// alert(3+3); //only in browser
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

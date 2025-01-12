b164b0388778327724e168af9322da3254141436
Gurnoor Singh Brar <157210409+GodlyLearner404@users.noreply.github.com>
2025-01-12 17:45:51 +0000

    conversions

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+// node 01_Basics/03_conversionOperations.js
+let score = "45a"
+
+// console.log(typeof score);
+
+let valInNumber = Number(score)
+
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+// "33" => 33
+// "42da" => NaN
+// true => 1/false => 0
+
+let isLoggedIn = " ";
+
+let boolLogged = Boolean(isLoggedIn);
+
+// console.log(typeof boolLogged);
+// console.log(boolLogged);
+
+// 1 => true / 0 => false
+// "" => false
+// "abc" => true
+
+let bool = false;
+let stringbool = String(bool);
+
+console.log(stringbool);
+

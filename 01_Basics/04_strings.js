6204a911468f49e77cb10d287227ac1c036b1374
Gurnoor Singh Brar <157210409+GodlyLearner404@users.noreply.github.com>
2025-01-16 05:42:43 +0000

    string intro

---
+++ 01_Basics/04_strings.js
@@ -0,0 +1,8 @@
+// node 01_Basics/04_strings.js
+
+const name = "Gurnoor"
+const repoCount = 12
+
+// console.log(name + repoCount + " Brar");
+
+console.log(`Hello I am ${name} and repo count is ${repoCount}`);
